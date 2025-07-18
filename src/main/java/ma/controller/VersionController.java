7d2e9dd8ae9a23df579d41636ac33f0d2d091955
Abdesselam Sectani <161614043+Sectani@users.noreply.github.com>
2025-07-18 16:11:25 +0100

    Update VersionController.java

--- src/main/java/ma/controller/VersionController.java
+++ src/main/java/ma/controller/VersionController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v2")
 public class VersionController {
     @GetMapping
     public ResponseEntity<String> versionTeller(){
-        return ResponseEntity.ok("Version 1 of this application running via Kubernetes");
+        return ResponseEntity.ok("Version 2 of this application running via Kubernetes");
     }
 }
